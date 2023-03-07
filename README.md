988c6e1767ba15568e0b062360e5bdc7d1632a65
Ross Merrigan <email@rossmerrigan.com>
2023-03-07 10:56:27 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# bloomfilter
+# Bloom Filter
+
+## Usage
+
+define your struct implemting the `Hash` interface defined in this module.
+
+```go
+type Test struct {
+
+}
+
+func (s *Test) Sum() int {
+  // your hash function...
+}
+```
+
+```go
+obj := &Test{}
+
+filter := bloomfilter.NewFilterOptimal[Test](2000000)
+filter.Add(obj)
+
+match := filter.Contains(obj)
+fmt.Println(match) // true
+```
--- filter.go
+++ filter.go
@@ -19,8 +19,9 @@ type Filter[T Hash] struct {
 	hash   HashFunction[T]
 }
 
-// Crates a new Bloom filter, using the optimal size for the underlying data structure based on the desired capacity and error rate,
-// as well as the optimal number of hash functions.
+// New Bloom filter
+// m The number of elements in the BitVector
+// k The number of hash functions to use
 func NewFilter[T Hash](capacity int, errorRate float64, hash HashFunction[T], m, k int) *Filter[T] {
 	if capacity < 1 {
 		panic("capacity must be > 0")
@@ -38,12 +39,26 @@ func NewFilter[T Hash](capacity int, errorRate float64, hash HashFunction[T], m,
 	}
 }
 
-// Creates a new Bloom filter, using the optimal size for the underlying data structure based on the desired capacity and error rate,
-// as well as the optimal number of hash functions.
-func NewFilterOptimal[T Hash](capacity int, errorRate float64, hash HashFunction[T]) *Filter[T] {
+// New Bloom filter using the optimal size based on the capacity and error rate
+func NewFilterOptimalWithErrorRate[T Hash](capacity int, errorRate float64, hash HashFunction[T]) *Filter[T] {
 	return NewFilter(capacity, errorRate, hash, int(bestM(capacity, errorRate)), int(bestK(capacity, errorRate)))
 }
 
+// New Bloom filter using the optimal size based on the capacity
+func NewFilterOptimal[T Hash](capacity int, hash HashFunction[T]) *Filter[T] {
+	return NewFilterOptimalWithErrorRate(capacity, bestErrorRate(capacity), hash)
+}
+
+func bestErrorRate(capacity int) float64 {
+	c := 1.0 / capacity
+	if c != 0 {
+		return float64(c)
+	}
+
+	// default
+	// http://www.cs.princeton.edu/courses/archive/spring02/cs493/lec7.pdf
+	return math.Pow(0.6185, float64(math.MaxInt32/capacity))
+}
 func bestK(capacity int, errorRate float64) float64 {
 	return math.Round(math.Log(2.0) * bestM(capacity, errorRate) / float64(capacity))
 }
@@ -53,7 +68,7 @@ func bestM(capacity int, errorRate float64) float64 {
 	return math.Ceil(float64(capacity) * math.Log(errorRate))
 }
 
-// The true bits.
+// The number of true bits.
 func (s *Filter[T]) TrueBits() int {
 	output := 0
 	iterator := s.vector.Enumerate()
@@ -67,7 +82,7 @@ func (s *Filter[T]) TrueBits() int {
 
 }
 
-// The ratio of false to true bits in the filter. E.g., 1 true bit in a 10 bit filter means a truthiness of 0.1.
+// The ratio of false to true bits in the BitVector.
 func (s *Filter[T]) Truthiness() float64 {
 	return float64(s.TrueBits()) / float64(s.vector.Length())
 }
